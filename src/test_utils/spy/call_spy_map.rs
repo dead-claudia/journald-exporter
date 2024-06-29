ca21d2d8cd8eb7684317105ef50d71adaee3bb5f
Claudia Meadows <contact@claudiameadows.dev>
2024-06-29 15:15:41 -0700

    Update call_spy_map.rs

--- src/test_utils/spy/call_spy_map.rs
+++ src/test_utils/spy/call_spy_map.rs
@@ -137,7 +137,7 @@ impl<K: PartialEq, I, O> CallSpyMap<K, I, O> {
             'outer: for state in states {
                 for (key, expected) in expected_map {
                     if *key == &state.key {
-                        if !state.args.iter().eq(expected.iter()) {
+                        if !state.args.iter().eq(&expected[..]) {
                             return false;
                         }
 
